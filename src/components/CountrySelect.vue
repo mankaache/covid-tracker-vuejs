86716130b8259de369e6a36c5a33e003e07c465e
mankaa <mankaache10@gmail.com>
2023-01-17 19:51:15 +0100

    displaying the countries

---
+++ src/components/CountrySelect.vue
@@ -0,0 +1,23 @@
+<template>
+    <select v-model="select" class="form-select mt-10 block w-full border p-3 rounded">
+        <option value="0">Select Country</option>
+        <option v-for="(country, index) in countries" :key="index" :value="country.ID">
+            {{country.Country}}
+        </option>
+    </select>
+</template>
+
+<script>
+    export default {
+        props:['countries'],
+        data() {
+            return {
+                select:0
+            }
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,54 +1,52 @@
 <template>
-    <main v-if="!loading">
-       <data-title :text="title" :dataDate="dataDate"></data-title>
-       <data-boxes :stats="stats"></data-boxes>
-    </main>
-    <main v-else class="flex flex-col align-center justify-center text-center " >
-      <div class="text-gray-500 text-3xl mt-10 mb-6">
-        Fetching Data
-      </div>
-      <img :src="loadingImage" class="w-24 m-auto" alt="img">
-    </main>
+  <main v-if="!loading">
+    <data-title :text="title" :dataDate="dataDate"></data-title>
+    <data-boxes :stats="stats"></data-boxes>
+    <country-select :countries="countries"></country-select>
+  </main>
+  <main v-else class="flex flex-col align-center justify-center text-center">
+    <div class="text-gray-500 text-3xl mt-10 mb-6">Fetching Data</div>
+    <img :src="loadingImage" class="w-24 m-auto" alt="img" />
+  </main>
 </template>
 
 <script>
-import DataTitle from '@/components/DataTitle.vue'
-import DataBoxes from '@/components/DataBoxes.vue'
-  export default {
-    components:{
-      DataTitle,
-      DataBoxes
+import DataTitle from "@/components/DataTitle.vue";
+import DataBoxes from "@/components/DataBoxes.vue";
+import CountrySelect from "@/components/CountrySelect.vue";
+export default {
+  components: {
+    DataTitle,
+    DataBoxes,
+    CountrySelect,
+  },
+  data() {
+    return {
+      loading: true,
+      title: "Global",
+      dataDate: "",
+      stats: {},
+      countries: [],
+      loadingImage: require("../assets/icons8-hourglass.gif"),
+    };
+  },
+  methods: {
+    async fetchCovidData() {
+      const response = await fetch("https://api.covid19api.com/summary");
+      const data = await response.json();
+      console.log(data);
+      return data;
     },
-    data() {
-      return {
-        loading:true,
-        title:'Global',
-        dataDate:"",
-        stats:{},
-        countries:[],
-        loadingImage:require('../assets/icons8-hourglass.gif')
-
-      }
-    },
-    methods:{
-      async fetchCovidData(){
-        const response = await fetch('https://api.covid19api.com/summary')
-        const data = await response.json()
-       console.log(data)
-        return data
-      }
-    },
-    async created(){
-      const data  = await this.fetchCovidData() 
-      console.log(data)
-      this.dataDate = data.Date
-      this.stats = data.Global
-      this.countries = data.Countries
-      this.loading = false
-    }
-  }
+  },
+  async created() {
+    const data = await this.fetchCovidData();
+    console.log(data);
+    this.dataDate = data.Date;
+    this.stats = data.Global;
+    this.countries = data.Countries;
+    this.loading = false;
+  },
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
