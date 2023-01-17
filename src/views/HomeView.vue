259e641f14e3a26126bbf1f2b28a1575bf14c210
mankaa <mankaache10@gmail.com>
2023-01-17 20:41:02 +0100

    completed

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,8 @@
     <data-title :text="title" :dataDate="dataDate"></data-title>
     <data-boxes :stats="stats"></data-boxes>
     <country-select @get-country="getCountryData" :countries="countries"></country-select>
+
+    <button v-if="stats.Country" @click="clearData"> Clear Country</button>
   </main>
   <main v-else class="flex flex-col align-center justify-center text-center">
     <div class="text-gray-500 text-3xl mt-10 mb-6">Fetching Data</div>
@@ -40,6 +42,13 @@ export default {
     getCountryData(country){
       this.stats = country
       this.title = country.Country
+    },
+    async clearData(){
+      this.loading = true
+      const data = await this.fetchCovidData()
+      this.title = 'Global'
+      this.stats = data.Global
+      this.loading = false
     }
   },
   async created() {
@@ -53,4 +62,46 @@ export default {
 };
 </script>
 
-<style scoped></style>
+
+
+<style scoped>
+button {
+ --green: blue;
+ font-size: 15px;
+ padding: 0.7em 2.7em;
+ letter-spacing: 0.06em;
+ position: relative;
+ font-family: inherit;
+ border-radius: 0.6em;
+ overflow: hidden;
+ font-weight:700;
+ font-size:20px;
+ margin-top:30px;
+ transition: all 0.3s;
+ line-height: 1.4em;
+ border: 2px solid var(--green);
+ background: linear-gradient(to right, rgba(27, 253, 156, 0.1) 1%, transparent 40%,transparent 60% , rgba(27, 253, 156, 0.1) 100%);
+ color: var(--green);
+ box-shadow: inset 0 0 10px rgba(27, 253, 156, 0.4), 0 0 9px 3px rgba(27, 253, 156, 0.1);
+}
+
+button:hover {
+ color: #161817;
+
+}
+
+button:before {
+ content: "";
+ position: absolute;
+ left: -4em;
+ width: 4em;
+ height: 100%;
+ top: 0;
+ transition: transform .4s ease-in-out;
+ background: linear-gradient(to right, transparent 1%, rgba(27, 253, 156, 0.1) 20%,rgba(27, 253, 156, 0.1) 30% , transparent 100%);
+}
+
+button:hover:before {
+ transform: translateX(15em);
+}
+</style>
